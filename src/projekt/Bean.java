32560c0baee4d09365cba6ab7675f2d1e1593dcc
zigalepi <ziga.susin@student.um.si>
2019-05-13 14:01:52 +0200

    1

---
+++ src/projekt/Bean.java
@@ -0,0 +1,13 @@
+package projekt;
+
+import java.io.Serializable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bean implements Serializable{
+
+	Logger log=LoggerFactory.getLogger(Bean.class);
+
+	
+}
