2b35731fead38ae456de2580a5e97261c56726e9
GasperReher <gasper.reher@student.um.si>
2019-05-27 12:17:07 +0200

    persistence

--- src/META-INF/create.sql
+++ src/META-INF/create.sql
@@ -2,4 +2,5 @@ insert into knjiga values (null,"joze","gasper na jasi","","amsdmas","knjiga")
 insert into knjiga values (null,"gasper","joze na jasi","","dasdasd","knjiga")
 
 insert into knjiga values (null,"david","js sm noob","","12312ff","knjiga")
+insert into Uporabnik values (null,"","admin","admin","admin","123","admin")
 
